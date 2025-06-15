9e8c987a6e4d50a76e79fac4efb935455b39fce2
TCWU100236 <hola7671@gmail.com>
2025-06-15 23:07:04 +0800

    backend update

--- iim_assets/iim_assets/urls.py
+++ iim_assets/iim_assets/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('detail/<int:id>', views.detail, name = 'detail-url'),
     path('asset_user/', views.asset_user),
-    path('asset_user/InsertAssetUser/', views.insert_asset_user),
-    path('insert/', views.insert),
+    path('insert/', views.asset_insert),
     path('', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- iim_assets/mysite/views.py
+++ iim_assets/mysite/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from mysite import models, forms
 
 # Create your views here.
 def index(request):
+    users = models.UserProfile.objects.all()
+
     # 取得 GET 參數
     asset_type = request.GET.get("asset_type")
     search_asset_id = request.GET.get("search_asset_id")
@@ -10,13 +12,10 @@ def index(request):
 
     # 建立查詢集（QuerySet）
     assets = models.Asset.objects.all()
-
     if asset_type:
         assets = assets.filter(asset_type=asset_type)
-
     if search_asset_id:
         assets = assets.filter(asset_code__icontains=search_asset_id)
-
     if user_id:
         assets = assets.filter(user__userid__icontains=user_id)
 
@@ -33,13 +32,31 @@ def detail(request, id):
 
 def asset_user(request):
     asset_users = models.UserProfile.objects.all()
+
+    if request.method == "POST":
+        asset_user_form = forms.UserProfileForm(request.POST)
+        if asset_user_form.is_valid():
+            asset_user_form.save()
+            return redirect("/asset_user/")
+        else:
+            message = "財產使用者新增失敗"
+    else:
+        asset_user_form = forms.UserProfileForm()
+
     return render(request, "asset_user.html", locals())
 
-def insert_asset_user(request):
-    asset_user_form = forms.UserProfileForm()
-    return render(request, "InsertAssetUser.html", locals())
-
-def insert(request):
-    asset_form = forms.AssetForm()
-    users = models.UserProfile.objects.all()
-    return render(request, "insert.html", locals())
+def asset_insert(request):
+    message = ""
+    if request.method == "POST":
+        asset_form = forms.AssetForm(request.POST)
+        if asset_form.is_valid():
+            asset_form.save()
+            return redirect("/")
+        else:
+            message = "資料有錯，請再檢查一次"
+            for field, errors in asset_form.errors.items():
+                for error in errors:
+                    message += f"\n{field}: {error}"
+    else:
+        asset_form = forms.AssetForm()
+    return render(request, "InsertAsset.html", locals())
